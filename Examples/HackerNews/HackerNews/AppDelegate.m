07987e49e24427687265460dea168cedd9f8ac09
Tomaz Nedeljko <tomaz.nedeljko@gmail.com>
2012-12-14 00:04:02 +0100

    Repo fixes

--- Examples/HackerNews/HackerNews/AppDelegate.m
+++ Examples/HackerNews/HackerNews/AppDelegate.m
@@ -8,17 +8,35 @@
 
 #import "AppDelegate.h"
 
+<<<<<<< HEAD
+=======
+NSString * const kInstapaperAPIClientIdentifier = @"";
+NSString * const kInstapaperAPIClientSecret = @"";
+NSString * const kPocketAPIClientIdentifier = @"";
+NSString * const kReadabilityAPIClientIdentifier = @"";
+NSString * const kReadabilityAPIClientSecret = @"";
+
+>>>>>>> 75119ff... Added support for authorization with OAuth 2.0 services.
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Set your client identifiers and secrets if you want to test read later services.
     [[AFNetworkActivityIndicatorManager sharedManager] setEnabled:YES];
+<<<<<<< HEAD
     [[NNInstapaperClient sharedClient] setClientIdentifier:@""];
     [[NNInstapaperClient sharedClient] setClientSecret:@""];
     [[NNPocketClient sharedClient] setAPIKey:@""];
     [[NNReadabilityClient sharedClient] setClientIdentifier:@""];
     [[NNReadabilityClient sharedClient] setClientSecret:@""];
+=======
+    [[NNInstapaperClient sharedClient] setClientIdentifier:kInstapaperAPIClientIdentifier];
+    [[NNInstapaperClient sharedClient] setClientSecret:kInstapaperAPIClientSecret];
+    [[NNPocketClient sharedClient] setClientIdentifier:kPocketAPIClientIdentifier];
+    [[NNPocketClient sharedClient] setScheme:@"pocketapp-hacker-news"];
+    [[NNReadabilityClient sharedClient] setClientIdentifier:kReadabilityAPIClientIdentifier];
+    [[NNReadabilityClient sharedClient] setClientSecret:kReadabilityAPIClientSecret];
+>>>>>>> 75119ff... Added support for authorization with OAuth 2.0 services.
     return YES;
 }
 
